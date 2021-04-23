14abd17e96cb686ae3ab460dcc57eb6af509739b
Jeffrey Zhang <jzhang11235@gmail.com>
2021-04-23 22:35:44 +1000

    entry structs

--- convert.go
+++ convert.go
@@ -18,6 +18,26 @@ type Entity struct {
 	Value string `gorm:"notNull"`
 }
 
+type Entry struct {
+	Sequence int       `xml:"ent_seq" gorm:"primaryKey;notNull"`
+	Kanji    []Kanji   `xml:"k_ele"`
+	Reading  []Reading `xml:"r_ele"`
+}
+
+type Kanji struct {
+	Element  string   `xml:"keb"`
+	Info     []string `xml:"ke_inf"`
+	Priority []string `xml:"ke_pri"`
+}
+
+type Reading struct {
+	Element     string    `xml:"reb"`
+	NoKanji     *struct{} `xml:"re_nokanji"` // nil means false
+	Restriction []string  `xml:"re_restr"`
+	Info        []string  `xml:"re_inf"`
+	Priority    []string  `xml:"re_pri"`
+}
+
 func main() {
 	f, err := os.Open("sample.xml")
 	if err != nil {
@@ -55,8 +75,8 @@ func main() {
 				// Document type definition (DTD)
 				// Create a dummy entity map for the decoder, which maps the entity name
 				// to the name itself, e.g. {"key1": "key1", "key2": "key2"}.
-				// This is so the decoder can interpret those entities, and the
-				// unmarshaler simply stores them by name.
+				// This is so the decoder can interpret those entities, and store them
+				// by name when unmarshaling.
 				// Also create an entity slice for GORM to store in DB
 				m := make(map[string]string)
 				var s []Entity
@@ -68,6 +88,11 @@ func main() {
 				dec.Entity = m
 
 				db.CreateInBatches(s, len(s))
+			}
+		case xml.StartElement:
+			if t.Name.Local == "entry" {
+				var e Entry
+				dec.DecodeElement(&e, &t)
 			}
 		}
 	}
